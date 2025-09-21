966cfa5914adc4b2f216b236a445620ca9df6bbd
developersushan <sushansing709sushansing@gmail.com>
2025-09-22 02:26:06 +0600

    register page

--- innovatun/src/routes/routes.tsx
+++ innovatun/src/routes/routes.tsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "../pages/Home/Home";
 import Main from "../layout/Main/Main";
+import About from "../pages/About/About";
+import Register from "../features/auth/Register/Register";
 
 const router = createBrowserRouter([
   {
@@ -10,6 +12,18 @@ const router = createBrowserRouter([
       {
         path: "/",
         element: <Home />,
+      },
+      {
+        path: "/home",
+        element: <Home />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+            {
+        path: "/register",
+        element: <Register />,
       },
     ],
   },
