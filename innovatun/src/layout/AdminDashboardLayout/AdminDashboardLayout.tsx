fdf3c5e93fa0b097b3c50db94de4b01f7168fe9b
AyshaUrmi0 <ayshaismail021@gmail.com>
2025-09-29 23:14:49 +0600

    Remove AdminHeader from AdminDashboardLayout

--- innovatun/src/layout/AdminDashboardLayout/AdminDashboardLayout.tsx
+++ innovatun/src/layout/AdminDashboardLayout/AdminDashboardLayout.tsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
 import AdminSidebar from "../../components/Admin/AdminSidebar";
-import AdminHeader from "../../components/Admin/AdminHeader";
+
 
 export default function AdminDashboardLayout() {
   return (
@@ -8,13 +8,10 @@ export default function AdminDashboardLayout() {
       {/* Admin Sidebar */}
       <AdminSidebar />
 
-      {/* Main Content */}
+
       <main className="flex-1 bg-gray-50">
-        <div>
-          <AdminHeader />
-        </div>
         <div className="bg-[#d9d9d9] min-h-full">
-          <Outlet /> {/* child pages render here */}
+          <Outlet /> 
         </div>
       </main>
     </div>
