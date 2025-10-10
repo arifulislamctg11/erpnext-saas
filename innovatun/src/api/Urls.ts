ff382ecc8cc69d21915dd758e0f950dd32f93c16
AyshaUrmi0 <ayshaismail021@gmail.com>
2025-10-11 03:56:57 +0600

    update baseUrl in Urls.ts and refresh hosting cache

--- innovatun/src/api/Urls.ts
+++ innovatun/src/api/Urls.ts
@@ -1,5 +1,5 @@
-export const baseUrl='http://localhost:5000'
-// export const baseUrl='https://backend-ten-red-40.vercel.app'
+// export const baseUrl='http://localhost:5000'
+export const baseUrl='https://backend-ten-red-40.vercel.app'
 
 export const SendOTPUrl = '/sendotp';
 
