7ae753e6465cee315f1111f3ee8ffa6db07b524a
Aljoboyer <tanveeazmain@gmail.com>
2025-10-11 16:25:25 +0600

    done

--- innovatun/src/pages/Dashboard/DashboardHome/components/dailog/form.tsx
+++ innovatun/src/pages/Dashboard/DashboardHome/components/dailog/form.tsx
@@ -86,9 +86,8 @@ export default function TableFrom({ setOpen, companyName, setRefetchEmployee }: 
     setLoading(true);
     const formData = new FormData(e.target as HTMLFormElement);
     const data = Object.fromEntries(formData.entries());
-    const newData = { ...data, companyName: companyName }
+    const newData: any = { ...data, companyName: companyName }
  
-
     const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z0-9]).{8,}$/;
 
     if (!passwordRegex.test(newData.password as string)) {
@@ -106,7 +105,7 @@ export default function TableFrom({ setOpen, companyName, setRefetchEmployee }: 
     try {
 
         const plan_roles = currentPlanData?.access_roles;
-        console.log(plan_roles)
+        
         const rolesArray = getEnabledRoles(accessRoles, plan_roles);
         const uniqueArray = [...new Set(rolesArray)];
         const formatRoles = uniqueArray?.map((item: any) => {
@@ -124,7 +123,7 @@ export default function TableFrom({ setOpen, companyName, setRefetchEmployee }: 
         
         const roleReqBody = {
           roles: formatRoles,
-          email: user?.email,
+          email: newData?.email,
           blockRoles
         }
       const res = await axios.post(`${baseUrl}/create-user-and-employee`, newData);
@@ -150,6 +149,7 @@ export default function TableFrom({ setOpen, companyName, setRefetchEmployee }: 
   }
 
   const getCurrentPlan = async () => {
+     setLoading(true);
     const response = await fetch(`${api.baseUrl}${CurrentPlanUrl}?email=${user?.email}`, {
               method: "GET",
               headers: { "Content-Type": "application/json" },
@@ -169,9 +169,11 @@ export default function TableFrom({ setOpen, companyName, setRefetchEmployee }: 
     if(formatedRes?.data){
       const findPlan = formatedPlanRes?.products?.find((item : any) => item?.name == formatedRes?.data?.planName)
       setCurrentPlanData(findPlan);
+       setLoading(false);
 
     }else{
       setCurrentPlanData(null)
+       setLoading(false);
     }
   }
   
