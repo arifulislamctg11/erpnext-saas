dbe5f345f797e013bd46e45e7edac3cbd0017f28
Aljoboyer <tanveeazmain@gmail.com>
2025-10-14 16:54:33 +0600

    d

--- innovatun/src/pages/Shared/UserDetails.tsx
+++ innovatun/src/pages/Shared/UserDetails.tsx
@@ -20,6 +20,23 @@ import {
 export default function UserDetail() {
   const { id } = useParams();
   const navigate = useNavigate();
+  const [userDetails, setUserDetails]: any = useState(null);
+
+    const getUser = async () => {
+      const response = await fetch(`${api.baseUrl}/single-user?email=${id}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+      });
+      const formatedRes = await response.json();
+      console.log(formatedRes?.data?.data);
+      setUserDetails(formatedRes?.data?.data)
+  }
+  useEffect(() => {
+    getUser()
+  },[]);
 
 
   return (
@@ -35,7 +52,7 @@ export default function UserDetail() {
             <ArrowLeft className="h-4 w-4" />
             <span>Back</span>
           </Button>
-          {
+            {
              id ? <Button variant="outline" size="sm">
                 <ShieldBan className="h-4 w-4 mr-2" />
                 Disable
@@ -46,9 +63,10 @@ export default function UserDetail() {
              }
         </div>
       </div>
-       <Card>
+      {
+        userDetails?.first_name ?  <Card>
             <CardHeader>
-              <CardTitle>User Information {id}</CardTitle>
+              <CardTitle>User Information</CardTitle>
               <CardDescription>
                 Basic user details and contact information
               </CardDescription>
@@ -56,12 +74,12 @@ export default function UserDetail() {
             <CardContent className="space-y-4">
               <div className="flex items-center space-x-4">
                 <Avatar className="h-16 w-16">
-                  <AvatarImage src="" alt={'AHAN'} />
-                  <AvatarFallback>{('AAA').slice(0,2).toUpperCase()}</AvatarFallback>
+                  <AvatarImage src="" alt={userDetails?.first_name} />
+                  <AvatarFallback>{(userDetails?.first_name).slice(0,2).toUpperCase()}</AvatarFallback>
                 </Avatar>
                 <div>
-                  <div className="text-lg font-semibold">fasdfasfd</div>
-                  <div className="text-sm text-gray-500">fasdfasdfas</div>
+                  <div className="text-lg font-semibold">{userDetails?.full_name}</div>
+                  <div className="text-sm text-gray-500"></div>
                 </div>
               </div>
               <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
@@ -69,7 +87,7 @@ export default function UserDetail() {
                   <Mail className="h-5 w-5 text-gray-400" />
                   <div>
                     <p className="text-sm font-medium text-start">Email</p>
-                    <p className="text-sm text-gray-600">asdfsdfas</p>
+                    <p className="text-sm text-gray-600">{userDetails?.email}</p>
                   </div>
                 </div>
                 
@@ -85,20 +103,21 @@ export default function UserDetail() {
                   <Calendar className="h-5 w-5 text-gray-400" />
                   <div>
                     <p className="text-sm font-medium">Signup Date</p>
-                    <p className="text-sm text-gray-600">fsdfsdf</p>
+                    <p className="text-sm text-gray-600"></p>
                   </div>
                 </div>
                 
                 <div className="flex items-center space-x-3">
                   <Calendar className="h-5 w-5 text-gray-400" />
                   <div>
-                    <p className="text-sm font-medium">Last Login</p>
-                    <p className="text-sm text-gray-600">dgdsf</p>
+                    <p className="text-sm font-medium text-start">Last Login</p>
+                    <p className="text-sm text-gray-600">{new Date(userDetails?.last_login).toLocaleString()}</p>
                   </div>
                 </div>
               </div>
             </CardContent>
-          </Card>  
+      </Card>  : <h1>Loading....</h1>
+      }
     </div>
   );
 }
